f04aee182736886783fe83932143fd59f9db93a9
Michael Danaher <mikel.d@live.ie>
2022-10-15 13:19:38 -0400

    Added Media Queries for Project Cards for all device sizes

--- src/@lekoarts/gatsby-theme-cara/components/Projects.js
+++ src/@lekoarts/gatsby-theme-cara/components/Projects.js
@@ -4,23 +4,38 @@ import "./Projects.css"
 const Projects = () => {
     return (
         <div className="container">
+            <h1>Projects</h1>
             <div className="row">
-                <div className="col">
-                    <MyProjectCard
-                        title="RockAround: Final Year MyProjectCard"
+                <MyProjectCard
+                        title="RockAround: Final Year Project"
                         description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
                         href='https://github.com/mikel112543/FYP_RockAround'
                         bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <MyProjectCard
-                        title="Blockbuster: Movie Rental Microservice"
-                        description="An Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
-                        href='https://github.com/mikel112543/CS4227_Blockbuster'
-                        bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"/>
-                </div>
+                <MyProjectCard
+                        title="RockAround: Final Year Project"
+                        description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
+                        href='https://github.com/mikel112543/FYP_RockAround'
+                        bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
+                <MyProjectCard
+                    title="RockAround: Final Year Project"
+                    description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
+                    href='https://github.com/mikel112543/FYP_RockAround'
+                    bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
+                <MyProjectCard
+                    title="RockAround: Final Year Project"
+                    description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
+                    href='https://github.com/mikel112543/FYP_RockAround'
+                    bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
+                <MyProjectCard
+                    title="RockAround: Final Year Project"
+                    description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
+                    href='https://github.com/mikel112543/FYP_RockAround'
+                    bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
+                <MyProjectCard
+                    title="RockAround: Final Year Project"
+                    description="Final Year MyProjectCard Android application that allows Musicians and Venues to communicate and review each other. This project showcases the industry standard MVVM architectural pattern"
+                    href='https://github.com/mikel112543/FYP_RockAround'
+                    bg='linear-gradient(to right, #D4145A 0%, #FBB03B 100%)'/>
             </div>
         </div>
     )
