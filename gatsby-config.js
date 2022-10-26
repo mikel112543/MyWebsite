7cc1b12e846c34fa8bc77e3ddeeb077ea41fd3aa
Michael Danaher <michael.danaher112543@gmail.com>
2022-10-26 14:31:29 -0400

    small fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
  */
 module.exports = {
   siteMetadata: {
-    // You can overwrite values here that are used for the SEO component
-    // You can also add new values here to query them like usual
-    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
+    pathPrefix: `MyWebsite`,
     siteTitle: `Michael Danaher`,
     siteTitleAlt: `Michael Danaher`,
     siteHeadline: `Michael Danaher`,
