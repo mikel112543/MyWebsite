b224b0ef6c9e1370d1cffa95138a1bc534f21a2c
Michael Danaher <mikel.d@live.ie>
2022-10-27 16:25:00 -0400

    code cleanup

--- src/@lekoarts/gatsby-theme-cara/components/Intro.js
+++ src/@lekoarts/gatsby-theme-cara/components/Intro.js
@@ -5,12 +5,15 @@ import "./Intro.css"
 const Intro = () => {
     return (
         <div className="container">
-            <h1>Hi, my name is Michael 👋</h1>
-            <img className="profile-image" src={profileImage} alt='https://via.placeholder.com/525x200'/>
-            <p className="intro-text">Welcome to my website. Here you will find some projects that I have created.
-                Ranging from frontend development, backend development, mobile development, machine learning, as various algorithm-based projects.
-                You can also find more information about my background as well as contact details and social media accounts below if you would like to reach out.</p>
-        </div>
+            <div className="item">
+                <h1>Hi, my name is Michael 👋</h1>
+                <img className="profile-image" src={profileImage} alt='https://via.placeholder.com/525x200'/>
+                <p className="intro">Welcome to my website. Here you will find some projects that I have created.
+                    Ranging from frontend development, backend development, mobile development, machine learning, as various algorithm-based projects.
+                    You can also find more information about my background as well as contact details and social media accounts below if you would like to reach out.</p>
+
+            </div>
+            </div>
     )
 }
 
